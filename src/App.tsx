c4e1f696c60f0fb31fe0a278d7f2f60a115d874e
Joli Holmes <holmesjoli@gmail.com>
2023-03-30 19:31:17 -0400

    renamed files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 // @ts-ignore
 import Chapter1  from "./pages/Chapter1";
