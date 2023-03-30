8c4c142e14f4bc29722b4a0ea5a128d1ac104766
Joli Holmes <holmesjoli@gmail.com>
2023-03-30 19:34:29 -0400

    removed boilerplate css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import './App.css'
 // @ts-ignore
 import Chapter1  from "./pages/Chapter1";
 import "./utils/styles/style.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
