f5f374c37ccc7f4b6b06c5e91a98d69605fb7621
Joli Holmes <holmesjoli@gmail.com>
2023-03-30 20:44:18 -0400

    added title

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 // @ts-ignore
 import Chapter1  from "./pages/Chapter1";
 import "./utils/styles/style.scss";
+import Lightning from './assets/lightning.svg';
 
 function App() {
 
   return (
     <div className="App">
+      <div>
+        <h1>Flee</h1>
+        <h2>Mapping the Increasing Impacts of Wildfires in the West</h2>
+      </div>
         <Chapter1></Chapter1>
     </div>
   )
