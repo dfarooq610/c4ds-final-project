bb3e68e2ee961290fd8232b6f1efc62774cd55ec
Joli Holmes <holmesjoli@gmail.com>
2023-04-07 10:38:09 -0400

    added initial additional html framing to main app

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,31 @@ import Chapter1  from "./pages/Chapter1";
 import "./utils/styles/style.scss";
 // import { ReactComponent as Lightning } './assets/lightning.svg';
 
+function Header({}) {
+  return(
+    <header>
+        <h1>Flee</h1>
+        <h2>Mapping the Increasing Impacts of Wildfires in the West</h2>
+        <p className="Description">The fire starts from a lightning strike on July 15th, 2014 and ended on August 14th, 2014. However, impacts from the fire were felt much longer than the timeline of the official event.</p>
+    </header>
+  )
+}
+
+function Footer({}) {
+  return(
+    <footer></footer>
+  )
+}
+
 function App() {
 
   return (
     <div className="App">
-      <div className="Header">
-        <h1>Flee</h1>
-        <h2>Mapping the Increasing Impacts of Wildfires in the West</h2>
-        <p className="Description">The fire starts from a lightning strike on July 15th, 2014 and ended on August 14th, 2014. However, impacts from the fire were felt much longer than the timeline of the official event.</p>
-      </div>
+      <Header></Header>
+      <div className="Chapters">
         <Chapter1></Chapter1>
+      </div>
+      <Footer></Footer>
     </div>
   )
 }
