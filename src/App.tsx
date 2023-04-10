c07a38ee7624ed38a6e193dff3b616c954d9d520
Joli Holmes <holmesjoli@gmail.com>
2023-04-10 16:06:31 -0400

    adjusted layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,40 @@ import "./utils/styles/style.scss";
 // import { ReactComponent as Lightning } './assets/lightning.svg';
 
 function Header({}) {
+
+  // const hamburger = document.getElementsByClassName("hamburger")[0];
+  // const mobileNavs = document.getElementsByClassName("nav-links")[0];
+
+  // hamburger.addEventListener("click", () => {
+  //   mobileNavs.classList.toggle("active")
+  // })
+
   return(
     <header>
-        <h1>Flee</h1>
-        <h2>Mapping the Increasing Impacts of Wildfires in the West</h2>
-        <p>In July 2014, lightning ignited a small fire in the Methow Valley, a remote area in eastern part of Washington State. Weeks of hot and dry weather left the valley particularly susceptible to fires that summer. Fortunately, several local residents saw the fire and rushed to extinguish it.</p>
-        <p>How did a small contained fire turn into one of the largest fires in Washington State's history? Local Danny and Vicki Yanarella share their knowledge. </p>
+      <button className="hamburger"><img src="./src/assets/menu.svg" alt="menu toggle icon"/></button>
+      <div className="nav-links">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="#about">About Me</a></li>
+          <li><a href="#skill">Skills</a></li>
+        </ul>
+      </div>
     </header>
   )
 }
 
 function Introduction({}) {
+
   return(
     <div className="Introduction">
-      <Header/>
+      <article>
+          <h1>Flee</h1>
+          <h2>Mapping the Increasing Impacts of Wildfires in the West</h2>
+          <div>
+            <p>In July 2014, lightning ignited a small fire in the Methow Valley, a remote area in eastern part of Washington State. Weeks of hot and dry weather left the valley particularly susceptible to fires that summer. Fortunately, several local residents saw the fire and rushed to extinguish it.</p>
+            <p>How did a small contained fire turn into one of the largest fires in Washington State's history? Local Danny and Vicki Yanarella share their knowledge. </p>
+          </div>
+        </article>
       <img className="lightning" src="./src/assets/lightning.svg"/>
   </div>
   )
@@ -27,6 +47,7 @@ function Introduction({}) {
 function Landing({}) {
   return(
   <div className="Landing">
+      <Header/>
       <Introduction/>
   </div>)
 }
