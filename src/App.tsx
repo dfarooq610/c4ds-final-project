f6d9bbd7d013340c38c0e11791d66425ad61836b
Joli Holmes <holmesjoli@gmail.com>
2023-04-11 08:03:39 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import * as d3 from 'd3';
+import { useState, useEffect } from 'react'
 // @ts-ignore
 import Chapter1  from "./pages/Chapter1";
 import "./utils/styles/style.scss";
@@ -6,23 +7,31 @@ import "./utils/styles/style.scss";
 
 function Header({}) {
 
-  // const hamburger = document.getElementsByClassName("hamburger")[0];
-  // const mobileNavs = document.getElementsByClassName("nav-links")[0];
-
-  // hamburger.addEventListener("click", () => {
-  //   mobileNavs.classList.toggle("active")
+  // useEffect(() => {
+  //   const hamburger = d3.select(".ham");
+  //   const navsub = d3.select(".nav-sub");
+  
+  //   hamburger.addEventListener('click', () => {  
+  //     hamburger.classList.toggle("change")  
+  //     navsub.classList.toggle("nav-change")  
+  //   });
   // })
 
   return(
     <header>
-      <button className="hamburger"><img src="./src/assets/menu.svg" alt="menu toggle icon"/></button>
-      <div className="nav-links">
-        <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="#about">About Me</a></li>
-          <li><a href="#skill">Skills</a></li>
+      <nav className="navbar">  
+        <div className="title"></div>  
+          <div className="ham" >  
+            <span className="bar1"></span>
+            <span className="bar2"></span>
+            <span className="bar3"></span>
+          </div>
+        <ul className="nav-sub">  
+          <li className="list-item"><a href="#" className="nav-link">Chapter 1</a></li>
+          <li className="list-item"><a href="#" className="nav-link">Chapter 2</a></li>
+          <li className="list-item"><a href="#" className="nav-link">Chapter 3</a></li>
         </ul>
-      </div>
+      </nav>
     </header>
   )
 }
@@ -32,30 +41,31 @@ function Introduction({}) {
   return(
     <div className="Introduction">
       <article>
-          <h1>Flee</h1>
-          <h2>Mapping the Increasing Impacts of Wildfires in the West</h2>
-          <div>
-            <p>In July 2014, lightning ignited a small fire in the Methow Valley, a remote area in eastern part of Washington State. Weeks of hot and dry weather left the valley particularly susceptible to fires that summer. Fortunately, several local residents saw the fire and rushed to extinguish it.</p>
-            <p>How did a small contained fire turn into one of the largest fires in Washington State's history? Local Danny and Vicki Yanarella share their knowledge. </p>
-          </div>
-        </article>
+        <h1>Flee</h1>
+        <h2>Mapping the Increasing Impacts of Wildfires in the West</h2>
+        <div>
+          <p>In July 2014, lightning ignited a small fire in the Methow Valley, a remote area in eastern part of Washington State. Weeks of hot and dry weather left the valley particularly susceptible to fires that summer. Fortunately, several local residents saw the fire and rushed to extinguish it.</p>
+          <p>How did a small contained fire turn into one of the largest fires in Washington State's history? Local residents Danny and Vicki Yanarella share their knowledge. </p>
+        </div>
+      </article>
       <img className="lightning" src="./src/assets/lightning.svg"/>
-  </div>
+    </div>
   )
 }
 
 function Landing({}) {
   return(
-  <div className="Landing">
+    <div className="Landing">
       <Header/>
       <Introduction/>
-  </div>)
+    </div>
+  )
 }
 
 function Footer({}) {
   return(
     <footer>
-      <h4>By Jovi Dai, Joli Holmes, and Danish Farooq </h4>
+      <h4>By Jovi Dai, Joli Holmes, and Danish Farooq</h4>
     </footer>
   )
 }
