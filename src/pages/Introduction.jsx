77b96067539be69e831f293a2e7f7c5a4c19e06d
Joli Holmes <holmesjoli@gmail.com>
2023-04-13 21:59:03 -0400

    made header a components

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -1,38 +1,4 @@
-// @ts-ignore
-import * as d3 from 'd3';
-import { useEffect } from 'react'
-
-function Header({}) {
-
-    const hamburger = d3.select(".ham");
-    const navsub = d3.select(".nav-sub");
-
-    useEffect(() => {
-      hamburger.on("click", function(e, d) {
-        console.log(e, d)
-        hamburger.classList.toggle("change");
-        navsub.classList.toggle("nav-change");
-      });
-    });
-
-    return(
-        <header>
-            <nav className="navbar">
-            <div className="ham">
-                <span className="bar1"></span>
-                <span className="bar2"></span>
-                <span className="bar3"></span>
-            </div>
-            <ul className="nav-sub">
-                <li className="list-item"><a href="#Introduction" className="nav-link">Introduction</a></li>
-                <li className="list-item"><a href="#Chapter1" className="nav-link">Chapter 1</a></li>
-                <li className="list-item"><a href="#Chapter2" className="nav-link">Chapter 2</a></li>
-                <li className="list-item"><a href="#Chapter3" className="nav-link">Chapter 3</a></li>
-            </ul>
-            </nav>
-        </header>
-    );
-}
+import Header from "../components/Header";
 
 export default function Introduction() {
 
