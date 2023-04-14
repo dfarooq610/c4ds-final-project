07e4dc8bcf89ac410dd4531d57356ff7db9586db
Joli Holmes <holmesjoli@gmail.com>
2023-04-13 20:25:33 -0400

    updated introduction

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -44,7 +44,7 @@ export default function Introduction({}) {
                 <h2>Mapping the Increasing Impacts of Wildfires in the West</h2>
                 <div>
                 <p>In July 2014, lightning ignited a small fire in the Methow Valley, a remote area in eastern part of Washington State. Weeks of hot and dry weather left the valley particularly susceptible to fires that summer. Fortunately, several local residents saw the fire and rushed to extinguish it.</p>
-                <p>How did a small contained fire turn into one of the largest fires in Washington State's history? Local residents Danny and Vicki Yanarella share their knowledge. </p>
+                <p>How did a small contained fire turn into one of the largest fires in Washington State's history? Local residents share their knowledge. </p>
                 </div>
             </article>
             <img className="lightning" src="./assets/lightning.svg" alt="lightning strike"/>
