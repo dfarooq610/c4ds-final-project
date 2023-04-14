91778cb92e5dbd740eea8ce18cac263118332c6a
Joli Holmes <holmesjoli@gmail.com>
2023-04-13 22:46:15 -0400

    updates

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -4,16 +4,15 @@ export default function Introduction() {
 
     return(
         <section id="Introduction" className="DarkMode">
-            <article classname="ChapterContainer IntroductionContainer">
+            <article className="ChapterContainer">
                 <h1>To Flee or to Face</h1>
                 <h2>The lingering impacts from wildfires in the west after a decade has passed</h2>
                 <div>
-                <p>In July 2014, lightning ignited a small fire in the Methow Valley, a remote area in eastern part of Washington State. Weeks of hot and dry weather left the valley particularly susceptible to fires that summer. Fortunately, several local residents saw the fire and rushed to extinguish it.</p>
-                <p>How did a small contained fire turn into one of the largest fires in Washington State's history? Local residents share their knowledge. </p>
+                    <p>In July 2014, lightning ignited a small fire in the Methow Valley, a remote area in eastern part of Washington State. Weeks of hot and dry weather left the valley particularly susceptible to fires that summer. Fortunately, several local residents saw the fire and rushed to extinguish it.</p>
+                    <p>How did a small contained fire turn into one of the largest fires in Washington State's history? Local residents share their knowledge. </p>
                 </div>
             </article>
             <img className="lightning" src="./assets/lightning.svg" alt="lightning strike"/>
         </section>
       )
 }
-
