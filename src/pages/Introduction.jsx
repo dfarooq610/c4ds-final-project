9340d1bcb469bb68bb4da144bd435f25a76f62c0
Joli Holmes <holmesjoli@gmail.com>
2023-04-17 08:47:47 -0400

    adjusted queries

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -8,7 +8,7 @@ export default function Introduction() {
                 <h1>To Flee or to Face</h1>
                 <h2>The lingering impacts from wildfires in the west after a decade has passed</h2>
                 <div>
-                    <p>In July 2014, lightning ignited a small fire in the Methow Valley, a remote area in eastern part of Washington State. Weeks of hot and dry weather left the valley particularly susceptible to fires that summer. Fortunately, several local residents saw the fire and rushed to extinguish it.</p>
+                    <p>In July 2014, lightning ignited a small fire in the Methow Valley, a remote area in eastern Washington State. Weeks of hot and dry weather left the valley particularly susceptible to fires that summer. Fortunately, several local residents saw the fire and rushed to extinguish it.</p>
                     <p>How did a small contained fire turn into one of the largest fires in Washington State's history? Local residents share their knowledge. </p>
                 </div>
             </article>
