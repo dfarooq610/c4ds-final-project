14606f299c9e474cf35d27b2a6ec9f0a9bd6d972
Joli Holmes <holmesjoli@gmail.com>
2023-04-17 20:47:42 -0400

    updated quote colors

--- src/pages/Chapter3.jsx
+++ src/pages/Chapter3.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Flickity from "react-flickity-component";
 
-
 function Carousel() {
   return (
     <Flickity>
@@ -30,10 +29,7 @@ function Carousel() {
   );
 }
 
-
 export default function Chapter3() {
-
-
 
   return (
     <section id="Chapter3" className="DarkMode">
@@ -60,11 +56,11 @@ export default function Chapter3() {
         <p>Debbie Grable and George Grable shared insights about the local government's response in the aftermath of the disaster.</p>
         
         <blockquote>
-          <span className="soundcite" data-id={1494682795} data-start={652000} data-end={664000} data-plays={1}>
+          {/* <span className="soundcite" data-id={1494682795} data-start={652000} data-end={664000} data-plays={1}>
           As far as the actual government's response, the little towns here can do what
           they, but there is not much they don't have funds unless they get them from
           somewhere.
-          </span>
+          </span> */}
         </blockquote>
         
         <p>She said it was people's house, so it was never going to be fast enough, and she does not if how they were reimbursed after insurance from the government. <i>"Everyone's insurance rates have gone up because of the fire danger, and now we are in a dangerous place,"</i> Debbie Grable said.</p>
@@ -79,56 +75,16 @@ export default function Chapter3() {
         <p>Despite the devastation caused by the Carlton Complex Fire, the community has come together to support one another in rebuilding. The Carlton Complex Long Term Recovery Group and various volunteer organizations have provided aid and resources to those affected. The group's efforts have helped many families and individuals rebuild their homes and lives.</p>
         <p>While the scars of the fire will remain, the resilience and strength of the community in the face of adversity have been genuinely inspiring. The Carlton Complex Fire serves as a reminder of the importance of disaster preparedness and the need for ongoing support for those affected by natural disasters.</p>
         <p><i>"If I was at that moment again, I don't care that much about anything I own,"</i> Viki Yanarella said. She said she might grab her boxes of photos from when her kids were little if she had time. And those photos, she thinks, would be irreplaceable that everything else is replaceable.</p>
-        
+
         <br></br>
 
         <p><b>Click below to check them out: </b></p>
-          <iframe
-            width="80%"
-            height={200}
-            allow="autoplay"
-            title="Interview with Debbie Grable and George Grable"
-            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494881827&color=%23554936&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-          />
-          <div
-            style={{
-              fontSize: 15,
-              color: "#cccccc",
-              lineBreak: "anywhere",
-              wordBreak: "normal",
-              overflow: "hidden",
-              whiteSpace: "nowrap",
-              textOverflow: "ellipsis",
-              fontFamily:
-                "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
-              fontWeight: 100
-            }}
-          >
-            <a
-              href="https://soundcloud.com/zhaozhou-dai"
-              title="Zhaozhou Dai and Joli Holmes"
-              target="_blank"
-              style={{ fontSize: 15, color: "#cccccc", textDecoration: "none" }}
-            >
-              Zhaozhou Dai and Joli Holmes
-            </a>{" "}
-            ·{" "}
-            <a
-              href="https://soundcloud.com/zhaozhou-dai/interview-with-debbie-grable-and-george-grable"
-              title="Interview with Debbie Grable and George Grable"
-              target="_blank"
-              style={{ fontSize: 15, color: "#cccccc", textDecoration: "none" }}
-            >
-              Interview with Debbie Grable and George Grable
-            </a>
-          </div>
-
         <iframe
           width="80%"
           height={200}
-          title="Interview with Vicki Yanarella and Danny Yanarella"
           allow="autoplay"
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494898483&color=%239b7d5d&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          title="Interview with Debbie Grable and George Grable"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494881827&color=%23554936&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
         />
         <div
           style={{
@@ -144,24 +100,63 @@ export default function Chapter3() {
             fontWeight: 100
           }}
         >
-          <a
-            href="https://soundcloud.com/zhaozhou-dai"
-            title="Zhaozhou Dai and Joli Holmes"
-            target="_blank"
-            style={{ fontSize: 15, color: "#cccccc", textDecoration: "none" }}
-          >
-            Zhaozhou Dai and Joli Holmes
-          </a>{" "}
-          ·{" "}
-          <a
-            href="https://soundcloud.com/zhaozhou-dai/interview-with-vicki-yanarella-and-danny-yanarella"
-            title="Interview with Vicki Yanarella and Danny Yanarella"
-            target="_blank"
-            style={{ fontSize: 15, color: "#cccccc", textDecoration: "none" }}
-          >
-            Interview with Vicki Yanarella and Danny Yanarella
-          </a>
-        </div>
+        <a
+          href="https://soundcloud.com/zhaozhou-dai"
+          title="Zhaozhou Dai and Joli Holmes"
+          target="_blank"
+          style={{ fontSize: 15, color: "#cccccc", textDecoration: "none" }}
+        >
+          Zhaozhou Dai and Joli Holmes
+        </a>{" "}
+        ·{" "}
+        <a
+          href="https://soundcloud.com/zhaozhou-dai/interview-with-debbie-grable-and-george-grable"
+          title="Interview with Debbie Grable and George Grable"
+          target="_blank"
+          style={{ fontSize: 15, color: "#cccccc", textDecoration: "none" }}
+        >
+          Interview with Debbie Grable and George Grable
+        </a>
+      </div>
+      <iframe
+        width="80%"
+        height={200}
+        title="Interview with Vicki Yanarella and Danny Yanarella"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494898483&color=%239b7d5d&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      />
+      <div
+        style={{
+          fontSize: 15,
+          color: "#cccccc",
+          lineBreak: "anywhere",
+          wordBreak: "normal",
+          overflow: "hidden",
+          whiteSpace: "nowrap",
+          textOverflow: "ellipsis",
+          fontFamily:
+            "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
+          fontWeight: 100
+        }}
+      >
+        <a
+          href="https://soundcloud.com/zhaozhou-dai"
+          title="Zhaozhou Dai and Joli Holmes"
+          target="_blank"
+          style={{ fontSize: 15, color: "#cccccc", textDecoration: "none" }}
+        >
+          Zhaozhou Dai and Joli Holmes
+        </a>{" "}
+        ·{" "}
+        <a
+          href="https://soundcloud.com/zhaozhou-dai/interview-with-vicki-yanarella-and-danny-yanarella"
+          title="Interview with Vicki Yanarella and Danny Yanarella"
+          target="_blank"
+          style={{ fontSize: 15, color: "#cccccc", textDecoration: "none" }}
+        >
+          Interview with Vicki Yanarella and Danny Yanarella
+        </a>
+      </div>
     </article>
     </div>
   </section>
